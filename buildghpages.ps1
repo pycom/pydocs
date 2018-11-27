968afa078e38c36543905a807e126390901411b5
Paul Thornton <ariochofmelnibone@gmail.com>
2018-11-27 16:59:05 +0000

    fixup docs

--- buildghpages.ps1
+++ buildghpages.ps1
@@ -2,6 +2,7 @@ gitbook install --gitbook=3.2.3
 gitbook build --gitbook=3.2.3
 
 cp _book/chapter/pybytes.html _book/chapter/pybytes/index.html
+cp _book/chapter/pybytes.html _book/pybytes.html
 cp -R _book/* .
 git clean -fx node_modules
 git clean -fx _book
